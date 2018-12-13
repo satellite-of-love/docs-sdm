ae0f282baf4dd911846bb5f572824071bc52cea9
Jessica Kerr <jessitron@atomist.com>
2018-12-13 00:52:13 -0600

    Remove extra samples

--- lib/machine/mkdocsStrict.ts
+++ lib/machine/mkdocsStrict.ts
@@ -1,6 +1,6 @@
 import {
     ExecuteGoal,
-    GoalInvocation, spawnAndLog, spawnPromise, execPromise, ExecPromiseError
+    GoalInvocation, spawnAndLog, spawnPromise
 } from "@atomist/sdm";
 
 
@@ -35,59 +35,4 @@ export const executeMkdocsStrict: ExecuteGoal = async (inv: GoalInvocation) => {
         }
         return { code: 0 };
     });
-}
-
-
-async function demoExecPromise() {
-    try {
-        const dockerPushResult = await execPromise("docker", ["push", "anImageTag"]);
-        const description = `docker push completed successfully. 
-            Stdout: ${dockerPushResult.stdout}
-            Stderr: ${dockerPushResult.stderr}`;
-    } catch (e) {
-        const epe = e as ExecPromiseError;
-        if (e.error) {
-            // an exception happened starting it up
-            throw e;
-        }
-        const description = `Exit code: ${e.status}, stderr: ${e.stderr}`;
-    }
-}
-
-
-async function demoSpawnAndLog(inv: GoalInvocation) {
-    const dockerPushResult = await spawnAndLog(inv.progressLog,
-        "docker", ["push", "anImageTag"]);
-    if (dockerPushResult.error) {
-        return {
-            code: 1,
-            message: dockerPushResult.error.message
-        }
-    }
-    if (dockerPushResult.status !== 0) {
-        return {
-            code: dockerPushResult.status,
-            message: "See the log for output",
-        }
-    }
-    const description = `docker push completed successfully. 
-            Stdout: ${dockerPushResult.stdout}
-            Stderr: ${dockerPushResult.stderr}`;
-    // do stuff with output
-}
-
-
-async function demoSpawnPromise(inv: GoalInvocation) {
-    const dockerPushResult = await spawnPromise("docker", ["push", "anImageTag"]);
-    if (dockerPushResult.error) {
-        return { code: 1, message: dockerPushResult.error.message }
-    }
-    if (dockerPushResult.status !== 0) {
-        inv.addressChannels(`docker push failed on ${inv.id.sha} on ${inv.id.branch}: ${dockerPushResult.stderr}`);
-        return { code: dockerPushResult.status || 1, message: dockerPushResult.stderr }
-    }
-    const description = `docker push completed successfully. 
-            Stdout: ${dockerPushResult.stdout}
-            Stderr: ${dockerPushResult.stderr}`;
-    // do stuff with output
 }

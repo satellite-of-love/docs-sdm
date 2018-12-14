86baef1e410987520d724ad753663916d3544ce3
Jessica Kerr <jessitron@atomist.com>
2018-12-14 17:15:18 -0600

    Convert to use new doWithProject

--- lib/machine/mkdocsStrict.ts
+++ lib/machine/mkdocsStrict.ts
@@ -19,36 +19,35 @@ import {
     GoalInvocation,
     spawnAndLog,
     spawnPromise,
+    doWithProject,
+    ProjectAwareGoalInvocation,
+    ExecPromiseError,
+    ExecPromiseResult,
 } from "@atomist/sdm";
 
-export const executeMkdocsStrict: ExecuteGoal = async (inv: GoalInvocation) => {
-    return inv.configuration.sdm.projectLoader.doWithProject({
-        credentials: inv.credentials,
-        id: inv.id,
-        readOnly: true,
-        cloneOptions: { detachHead: true },
-    }, async project => {
-        const pipResult = await spawnAndLog(inv.progressLog,
-            "pip", ["install", "-r", "requirements.txt"], { cwd: project.baseDir });
-        if (pipResult.error || pipResult.code !== 0) {
-            // this is unexpected
-            const message = pipResult.error ? pipResult.error.message : "See the log for output";
-            return { code: pipResult.status || 2, message };
-        }
 
-        const mkdocsResult = await spawnPromise(
-            "mkdocs", ["build", "--strict"], { cwd: project.baseDir });
-        inv.progressLog.write(mkdocsResult.stdout);
-        inv.progressLog.write(mkdocsResult.stderr);
-        if (mkdocsResult.error) {
-            // this is an unexpected error
-            return { code: mkdocsResult.status || 2, message: mkdocsResult.error.message };
-        }
-        if (mkdocsResult.status !== 0) {
-            // this is an expected kind of error; it means the tests failed
-            inv.addressChannels(`mkdocs --strict failed on ${inv.id.sha} on ${inv.id.branch}: ${mkdocsResult.stderr}`);
-            return { code: mkdocsResult.status || 1, message: mkdocsResult.stderr };
-        }
-        return { code: 0 };
-    });
-};
+
+export const executeMkdocsStrict: ExecuteGoal = doWithProject(async (inv: ProjectAwareGoalInvocation) => {
+
+    const pipResult = await inv.spawn("pip", ["install", "-r", "requirements.txt"]);
+    if (pipResult.code !== 0) {
+        // this is unexpected
+        const message = pipResult.error ? pipResult.error.message : "See the log for output";
+        return { code: pipResult.status || 2, message };
+    }
+
+    var mkdocsResult: ExecPromiseError | ExecPromiseResult;
+    const errors: string[] = [];
+    try {
+        mkdocsResult = await inv.exec("mkdocs", ["build", "--strict"]);
+    } catch (e) {
+        const epe = e as ExecPromiseError;
+        inv.addressChannels(`mkdocs --strict failed on ${inv.id.sha} on ${inv.id.branch}: ${epe.message}`);
+        errors.push(epe.message);
+        mkdocsResult = epe;
+    }
+    inv.progressLog.write(mkdocsResult.stdout);
+    inv.progressLog.write(mkdocsResult.stderr);
+
+    return { code: errors.length };
+});
